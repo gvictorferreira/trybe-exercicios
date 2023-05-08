25763ee5202d390a8ef2abc6d6abaf182be5fe27
Victor Ferreira <gabriel-bm10@hotmail.com>
2023-05-07 22:09:12 -0300

    Exercicios introducao a  js dia 2

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -0,0 +1,243 @@
+
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+}
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  result += numbers[index];
+}
+
+console.log(result);
+
+let media = result/numbers.length
+
+console.log(media);
+if (media > 20) {
+    console.log("A media é maior que 20");
+} else {
+    console.log("A media é menor que 20");
+}
+
+let higherNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
+}
+
+console.log(higherNumber);
+
+
+let resultado = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    resultado += 1;
+  }
+}
+
+if (result === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(resultado);
+}
+
+
+let smallestNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] < smallestNumber) {
+    smallestNumber = numbers[index];
+  }
+}
+
+console.log(smallestNumber);
+
+
+let number = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  number.push(index);
+}
+
+console.log(number);
+
+for (let index = 0; index < number.length; index += 1) {
+    console.log(number[index] / 2);
+  };
+
+  let fatorial = 1;
+
+for (let index = 10; index > 0; index -= 1) {
+  fatorial *= index;
+}
+
+console.log(fatorial);
+
+let word = 'tryber';
+
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+
+console.log(reverseWord);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 1; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 1; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);*/
+
+//BÔNUS//
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
+
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] > numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+
+console.log(numbers);
+
+let newArray = [];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (index < (numbers.length - 1)) { 
+    newArray.push(numbers[index] * numbers[index + 1]);
+  } else {
+    newArray.push(numbers[index] * 2);
+  }
+}
+
+console.log(newArray);
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};*/
+
+
+
+/* let size = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+  console.log(inputLine);
+  inputLine = inputLine + symbol;
+}; */
+
+/*let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n - 1;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+};*/
+
+/*let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
+*/
+/*let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
+let symbol = '*';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
+    } else {
+      outputLine += ' ';
+    }
+  }
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}*/
+
+let divisors = 1;
+let numberToCheck = 31;
+
+for (let number = 2; number <= numberToCheck; number += 1) {
+  if (numberToCheck % number === 0) divisors += 1;
+}
+
+if (divisors === 2) console.log(numberToCheck + ' é primo');
+else console.log(numberToCheck + ' não é primo');
+
